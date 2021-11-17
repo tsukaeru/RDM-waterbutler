c4189fc76e228809491d8c7bffe01b8891789edc
KakeruMizuno <kmizuno@tsukaeru.net>
2021-11-17 21:17:36 +0900

    update validate_path method

--- waterbutler/providers/rushfiles/provider.py
+++ waterbutler/providers/rushfiles/provider.py
@@ -85,19 +85,25 @@ class RushFilesProvider(provider.BaseProvider):
                               base: WaterButlerPath,
                               name: str,
                               folder: bool=None) -> WaterButlerPath:
-        response = await self.make_request(
-            'GET',
-            self.build_url(str(self.share['id']), 'virtualfiles', base.identifier, 'children'),
-            expects=(200, ),
-            throws=exceptions.MetadataError
-        )
-        data = await response.json()
-        child_id, index = self._search_inter_id(data, name)
+        children_path_list = name.strip('/').split('/')
+        current_inter_id = self.share['id']
+        for i, child in enumerate(children_path_list):
+            response = await self.make_request(
+                'GET',
+                self.build_url(str(self.share['id']), 'virtualfiles', str(current_inter_id), 'children'),
+                expects=(200, 404,),
+                throws=exceptions.MetadataError,
+            )
+            if response.status == 404:
+                raise exceptions.NotFoundError(name)
+            res = await response.json()
+            current_inter_id, index = self._search_inter_id(res, child)
+            if not current_inter_id:
+                if i == len(children_path_list)-1:
+                    break
+                raise exceptions.NotFoundError(name)
 
-        if child_id is None:
-            raise exceptions.NotFoundError(name)
-
-        return base.child(name, _id=child_id, folder=folder)
+        return base.child(name, _id=current_inter_id, folder=folder)
 
     def can_duplicate_names(self) -> bool:
         return False
